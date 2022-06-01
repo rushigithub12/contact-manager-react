69eeff9d8c39f22e6d10506b03b91aec3c428adc
rushigithub12 <82263330+rushigithub12@users.noreply.github.com>
2022-06-01 22:36:29 +0530

    Update local storage app.js

--- contact-manager-app/contact-manager/src/App.js
+++ contact-manager-app/contact-manager/src/App.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import AddContact from './components/AddContact';
-import ContactCard from './components/ContactCard';
-import ContactList from './components/ContactList';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
 
 function App() {
+  const LOAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
-    console.log(contact)
-    setContacts([ ...contacts, contact ])
-  }
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
+
+  useEffect(() => {
+    const retrievedContacts = JSON.parse(
+      localStorage.getItem('contacts')
+    );
+    if (retrievedContacts){
+      setContacts(retrievedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact addContactHandler={ addContactHandler }/>
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} />
     </div>
   );
