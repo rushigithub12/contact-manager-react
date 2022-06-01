ff5fe5992222a6582705c77e37b638f5abeec6ff
rushigithub12 <82263330+rushigithub12@users.noreply.github.com>
2022-06-01 20:11:53 +0530

    init: initialize react app

---
+++ contact-manager-app/contact-manager/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import AddContact from './components/AddContact';
+import ContactCard from './components/ContactCard';
+import ContactList from './components/ContactList';
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact)
+    setContacts([ ...contacts, contact ])
+  }
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={ addContactHandler }/>
+      <ContactList contacts={contacts} />
+    </div>
+  );
+}
+
+export default App;
