09389888ea0f8de8d51aed9f45e486de726b6f72
rushigithub12 <82263330+rushigithub12@users.noreply.github.com>
2022-06-02 14:58:19 +0530

    Update new folder

--- contact-manager-app/contact-manager/src/App.js
+++ contact-manager-app/contact-manager/src/App.js
@@ -1,36 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import AddContact from "./components/AddContact";
-import ContactList from "./components/ContactList";
+
 
 function App() {
-  const LOAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
-
-  const addContactHandler = (contact) => {
-    console.log(contact);
-    setContacts([...contacts, contact]);
-  };
-
-  useEffect(() => {
-    const retrievedContacts = JSON.parse(
-      localStorage.getItem('contacts')
-    );
-    if (retrievedContacts){
-      setContacts(retrievedContacts);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(LOAL_STORAGE_KEY, JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
-    <div className="ui container">
-      <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />
+    <div className="main">
+      <h3>React crud operations</h3>
     </div>
   );
 }
